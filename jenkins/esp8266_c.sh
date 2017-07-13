de6b2f7ad6295e059d8f67c2811c4e5682b933cb
Roy Sprowl <roys@jetstreamsoftware.com>
2017-07-13 13:39:58 -0700

    testing gate

--- jenkins/esp8266_c.sh
+++ jenkins/esp8266_c.sh
@@ -1,3 +1,7 @@
 echo "hello world"
 export PATH=$ESP8266_TOOLS/xtensa-lx106-elf/bin:$PATH
-echo $PATH
+echo PATH: $PATH
+echo ESP8266_RTOS_SDK: $ESP8266_RTOS_SDK
+echo ESP8266_TOOLS: $ESP8266_TOOLS
+echo pwd: $(pwd)
+
