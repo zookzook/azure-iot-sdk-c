1b26a4d046fa5e9e6eea974d40e073d00f3d117f
Roy Sprowl <roys@jetstreamsoftware.com>
2017-07-13 14:25:57 -0700

    ch 1

--- jenkins/esp8266_c.sh
+++ jenkins/esp8266_c.sh
@@ -1,5 +1,5 @@
 echo "hello world"
-export PATH=$ESP8266_TOOLS/xtensa-lx106-elf/bin:$PATH
+#export PATH=$ESP8266_TOOLS/xtensa-lx106-elf/bin:$PATH
 echo PATH: $PATH
 echo ESP8266_RTOS_SDK: $ESP8266_RTOS_SDK
 echo ESP8266_TOOLS: $ESP8266_TOOLS
