6fbf0ab44f964f10772b8830186cb1ddd34a62da
Roy Sprowl <roys@jetstreamsoftware.com>
2017-07-13 17:51:01 -0700

    ...

--- build_all/esp8266/aa.sh
+++ build_all/esp8266/aa.sh
@@ -2,7 +2,7 @@ export SDK_PATH=/home/roys/workspace/ESP8266_RTOS_SDK
 export BIN_PATH=/home/roys/workspace/build
 export PATH=/home/roys/workspace/esp-open-sdk/xtensa-lx106-elf/bin:$PATH
 
-#make clean
+make clean
 
 
 
