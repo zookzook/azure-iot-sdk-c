4ec4f499609e311c124401fcb39181065ee27d50
Roy Sprowl <roys@jetstreamsoftware.com>
2017-07-16 10:31:55 -0700

    putting failing stuff last

--- jenkins/esp8266_c.sh
+++ jenkins/esp8266_c.sh
@@ -1,11 +1,11 @@
 echo "hello world"
-#export PATH=$ESP8266_TOOLS/xtensa-lx106-elf/bin:$PATH
 echo PATH: $PATH
 echo ESP8266_RTOS_SDK: $ESP8266_RTOS_SDK
 echo ESP8266_TOOLS: $ESP8266_TOOLS
 echo pwd: $(pwd)
 cd ..
 dir
+export PATH=$ESP8266_TOOLS/xtensa-lx106-elf/bin:$PATH
 xtensa-lx106-elf-gcc --version
 
 
