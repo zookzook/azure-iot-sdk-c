aecba156251e891738b4e2ca50f1a826737c64f1
Roy Sprowl <roys@jetstreamsoftware.com>
2017-07-24 08:45:57 -0700

    testing

--- build_all/esp8266/user/user_main.c
+++ build_all/esp8266/user/user_main.c
@@ -1,41 +1,12 @@
-/*
- * ESPRSSIF MIT License
- *
- * Copyright (c) 2015 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
- *
- * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
- * it is free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 
 #include "esp_common.h"
+#include "wifi_config.h"
 
-/******************************************************************************
- * FunctionName : user_rf_cal_sector_set
- * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
- *                We add this function to force users to set rf cal sector, since
- *                we don't know which sector is free in user's application.
- *                sector map for last several sectors : ABCCC
- *                A : rf cal
- *                B : rf init data
- *                C : sdk parameters
- * Parameters   : none
- * Returns      : rf cal sector
-*******************************************************************************/
+// The ESP8266 sdk shows user_rf_cal_sector_set as a required function
+// that must be included in all user_main files.
 uint32 user_rf_cal_sector_set(void)
 {
     flash_size_map size_map = system_get_flash_size_map();
@@ -72,17 +43,135 @@ uint32 user_rf_cal_sector_set(void)
 
     return rf_cal_sec;
 }
-/******************************************************************************
- * FunctionName : user_init
- * Description  : entry of user application, init user function here
- * Parameters   : none
- * Returns      : none
-*******************************************************************************/
+
+#include "esp_common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include "lwip/opt.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "openssl/ssl.h"
+#include "iothub_client_sample_mqtt.h"
+#include "lwip/apps/sntp.h"
+#include "lwip/apps/sntp_time.h"
+#include "uart.h"
+
+#define OPENSSL_DEMO_THREAD_NAME "ssl_demo"
+#define OPENSSL_DEMO_THREAD_STACK_WORDS 1024*2
+#define OPENSSL_DEMO_THREAD_PRORIOTY 6
+
+static os_timer_t timer;
+
+LOCAL void ICACHE_FLASH_ATTR mqtt_sample()
+{
+    while (1) {
+        struct ip_info info;
+        wifi_get_ip_info(STATION_IF, &info);
+        if (info.ip.addr != 0) {
+            break;
+        }
+        else {
+            vTaskDelay(10);
+        }
+    }
+    iothub_client_sample_mqtt_run();
+    vTaskDelete(NULL);
+}
+
+LOCAL void ICACHE_FLASH_ATTR wait_for_connection_ready(uint8 flag)
+{
+    unsigned char ret = 0;
+    struct ip_info ipconfig;
+    xTaskHandle openssl_handle;
+    os_timer_disarm(&timer);
+    ret = wifi_station_get_connect_status();
+
+    os_printf("ret %d\n", ret);
+
+    if (ret == STATION_GOT_IP) {
+        printf("azure iot program starts %d\n", system_get_free_heap_size());
+
+        int result;
+        result = xTaskCreate(mqtt_sample,
+            OPENSSL_DEMO_THREAD_NAME,
+            OPENSSL_DEMO_THREAD_STACK_WORDS,
+            NULL,
+            OPENSSL_DEMO_THREAD_PRORIOTY,
+            &openssl_handle);
+
+        if (result != pdPASS) {
+            os_printf("create thread %s failed\n", OPENSSL_DEMO_THREAD_NAME);
+            return;
+        }
+
+    }
+    else {
+        os_timer_disarm(&timer);
+        os_timer_setfn(&timer, (os_timer_func_t *)wait_for_connection_ready, NULL);
+        os_timer_arm(&timer, 2000, 0);
+    }
+}
+
+LOCAL void ICACHE_FLASH_ATTR configWiFi()
+{
+    uint8 ssid[] = { 'B','e','l','l','a','C','o','d','e','2','.','4','G' };
+    uint8 password[] = { 'p','a','s','s','w','o','r','d','o','r','d' };
+
+
+    struct station_config sta_conf;
+    wifi_set_opmode(STATION_MODE);
+    memset(sta_conf.ssid, 0, 32);
+    memset(sta_conf.password, 0, 64);
+    memset(sta_conf.bssid, 0, 6);
+    memcpy(sta_conf.ssid, ssid, sizeof(ssid));
+    memcpy(sta_conf.password, password, sizeof(password));
+    sta_conf.bssid_set = 0;
+    wifi_station_set_config(&sta_conf);
+
+    os_timer_disarm(&timer);
+    os_timer_setfn(&timer, (os_timer_func_t *)wait_for_connection_ready, NULL);
+    os_timer_arm(&timer, 2000, 0);
+}
+
+LOCAL void ICACHE_FLASH_ATTR startWiFi(const char* ssid, const char* password)
+{
+    uint8 ssid[] = { 's','s','i','d' };
+    uint8 password[] = { 'p','a','s','s','w','o','r','d' };
+
+
+    struct station_config sta_conf;
+    wifi_set_opmode(STATION_MODE);
+    memset(sta_conf.ssid, 0, sizeof(sta_conf.ssid));
+    memset(sta_conf.password, 0, sizeof(sta_conf.password));
+    memset(sta_conf.bssid, 0, sizeof(sta_conf.bssid));
+    memcpy(sta_conf.ssid, ssid, sizeof(ssid));
+    memcpy(sta_conf.password, password, sizeof(password));
+    sta_conf.bssid_set = 0;
+    // We don't want to save wifi params to flash each time, we boot,
+    // so wifi_station_set_config_current is used instead of wifi_station_set_config.
+    // This call starts the connection process.
+    wifi_station_set_config_current(&sta_conf);
+}
+
+LOCAL void ICACHE_FLASH_ATTR start_azure_iot()
+{
+    // configWiFi kicks off the process of connecting to the AP when 
+    // called from outside a task
+
+}
+
+// This is the entry point for the ESP8266 to launch the user task.
 void user_init(void)
 {
+    // The ESP8266 does not start up at a predictable baud rate,
+    // so the connected terminal will show garbage for a while until
+    // the uart_div_modify() takes effect. This is considered normal.
     uart_div_modify(0, UART_CLK_FREQ / 115200);
     printf("Waiting for baud change\n");
     printf("Waiting for baud change\n");
     printf("Waiting for baud change\n");
-    printf("SDK version:%s\n", system_get_sdk_version());
+    printf("ESP8266 RTOS SDK version:%s\n", system_get_sdk_version());
+    start_azure_iot();
 }
---
+++ build_all/esp8266/user/wifi_config.h
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef WIFI_CONFIG_DATA_H
+#define WIFI_CONFIG_DATA_H
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "password"
+
+#endif // WIFI_CONFIG_DATA_H
