e57be4f17a22df06b647bf63a474a293aebb7a31
Carter Tinney <catinney@microsoft.com>
2017-10-05 16:59:20 -0700

    Added initial sample

---
+++ dps_service_client/samples/dps_sc_sample/dps_sc_sample.c
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "dps_sc_client.h"
+
+int main()
+{
+    int result;
+    result = 0;
+
+    return result;
+}
